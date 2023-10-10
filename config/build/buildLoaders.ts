6c34358576e6f7b63bde2d28cb6b65efd53e953a
Евгений <120249357+Evgen4ikk@users.noreply.github.com>
2023-10-10 18:07:38 +0700

    added navigation bar, sidebar and page translation

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -4,10 +4,14 @@ import {BuildOptions} from "./types/config";
 
 export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
 
+    const svgLoader = {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+    }
+
     const cssLoaders = {
         test: /\.s[ac]ss$/i,
         use: [
-            // Creates `style` nodes from JS strings
             isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
             {
                 loader: "css-loader",
@@ -24,14 +28,24 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
         ],
     }
 
-    const
-        typescriptLoader = {
-            test: /\.tsx?$/,
-            use: 'ts-loader',
-            exclude: /node_modules/,
-        }
+    const typescriptLoader = {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+    }
+
+    const fileLoader = {
+        test: /\.(png|jpe?g|gif|woff2|woff)$/i,
+        use: [
+            {
+                loader: 'file-loader',
+            },
+        ],
+    }
 
     return [
+        fileLoader,
+        svgLoader,
         typescriptLoader,
         cssLoaders,
     ]
