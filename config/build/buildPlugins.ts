62c5c6f8198564972e8bea10950f0182e607feed
Евгений <120249357+Evgen4ikk@users.noreply.github.com>
2024-05-15 02:28:46 +0700

    add visual tool, fixscreens

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,11 +1,11 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import webpack from 'webpack';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { BuildOptions } from './types/config';
 
 export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
-    const plugins: webpack.WebpackPluginInstance[] = [
+    const plugins = [
         new HtmlWebpackPlugin({
             template: paths.html,
         }),
@@ -21,7 +21,9 @@ export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPlu
 
     if (isDev) {
         plugins.push(new webpack.HotModuleReplacementPlugin());
-        plugins.push(new BundleAnalyzerPlugin({ openAnalyzer: false }));
+        plugins.push(new BundleAnalyzerPlugin({
+            openAnalyzer: false,
+        }));
     }
 
     return plugins;
